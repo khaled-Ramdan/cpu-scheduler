b68d892981e879b4f30c2104ed43e15987ec464d
modessam <64746678+modessam@users.noreply.github.com>
2022-12-26 18:37:17 +0200

    Update shortestremainingtimefirst.cpp

--- shortestremainingtimefirst.cpp
+++ shortestremainingtimefirst.cpp
@@ -1,84 +1,5 @@
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-struct Process
-{
-    ll readyTime, pId, numberOfInstructions, IOPercent, insIdx, executionTime;
-    vector<bool>insType;//0=>cpu - 1=> io
-    Process()
-    {
-        readyTime = 0;
-        pId = 0;
-        numberOfInstructions = 0;
-        IOPercent = 0;
-        insIdx = 0;
-        executionTime = 0;
-    };
-    Process(ll id, ll instructionCount, ll iopercent, ll arrivalTime)
-    {
-        pId = id;
-        numberOfInstructions = instructionCount;
-        IOPercent = iopercent;
-        readyTime = arrivalTime;
-        insIdx = 0;
-        insType.assign(numberOfInstructions, 0);
-        randomizeIO();
-    }
-    void randomizeIO()
-    {
-        ll ioCount = IOPercent * numberOfInstructions / 100;
-        for (int i = 0; i < ioCount; i++)
-            insType[i] = 1;
-        executionTime = numberOfInstructions - ioCount;
-        random_shuffle(insType.begin(), insType.end());
-    }
-    bool operator< (const Process& p) const
-    {
-        if (p.readyTime == this->readyTime)
-            return p.executionTime < this->executionTime;
-        else
-            return p.readyTime < this->readyTime;
-    }
-};
-
-struct processMetaData
-{
-    ll  arrival = -1, completion = -1, firstTun = -1;
-    ll responseTime()
-    {
-        return firstTun - arrival;
-    }
-    ll turnAroundTime()
-    {
-        return completion - arrival;
-    }
-};
-
-void metaDataDisplay(map<ll, processMetaData>procData, ll numberOfProcesses)
-{
-    ll totalTurnAround = 0, totalResponse = 0;
-    for (auto& i : procData)
-    {
-        cout << "process with pid = " << i.first << "\n";
-        cout << "turn around time = " << " " << i.second.turnAroundTime() << " "
-            << "Response Time = " << i.second.responseTime() << "\n";
-        totalTurnAround += i.second.turnAroundTime();
-        totalResponse += i.second.responseTime();
-    }
-    cout << "\nAverage TurnAround Time = " << totalTurnAround / numberOfProcesses << " "
-        << "Average Response Time = " << totalResponse / numberOfProcesses << "\n";
-}
-void print(ll st, ll en, ll pid, bool fin = 0)
-{
-    if (~pid)
-        cout << "process with id " << pid << " from " << st << " to " << en << "\n";
-    else
-        cout << "free cpu from " << st << " " << en << "\n";
-    if (fin)
-        cout << "Process is treminated....\n";
-}
-
-void shortestRemainingTimeFirst(vector<Process>& v, ll IOTime)
+#include"STCF.h"
+void shortestRemainingTimeFirst(vector<Process>& v, ll IOTime,ll ITIME)
 {
     priority_queue<Process>app;
     map<ll, processMetaData>procData;
@@ -99,12 +20,12 @@ void shortestRemainingTimeFirst(vector<Process>& v, ll IOTime)
             continue;
         }
         if (proc.pId != currentProc.first) {
-            print(currentProc.second, cTime, currentProc.first, procData[currentProc.first].completion != -1);
+            print(currentProc.second, cTime, currentProc.first, procData[currentProc.first].completion != -1,0);
             currentProc = make_pair(proc.pId, proc.readyTime);
         }
         if (proc.readyTime > cTime)
         {
-            print(cTime, proc.readyTime - 1,  -1);
+            print(cTime, proc.readyTime - 1,  -1,0,0);
             cTime = proc.readyTime;
         }
         if (procData[proc.pId].firstTun == -1)procData[proc.pId].firstTun = cTime;
@@ -127,30 +48,10 @@ void shortestRemainingTimeFirst(vector<Process>& v, ll IOTime)
         if (proc.insIdx == proc.numberOfInstructions)
             procData[proc.pId].completion = proc.readyTime;
     }
-    print(currentProc.second, cTime, currentProc.first, 1);
+    print(currentProc.second, cTime, currentProc.first, 1,0);
     metaDataDisplay(procData, v.size());
 
 }
 
 
 
-
-
-
-
-int main()
-{
-    srand(time(0));
-    int noProcess, IOTime, InsExeTime;
-
-    cin >> noProcess >> IOTime >> InsExeTime;
-    vector<Process>v(noProcess);
-    for (int i = 0; i < noProcess; i++)
-    {
-        int pid, insCount, IOper, startTime;
-        cin >> pid >> insCount >> IOper >> startTime;
-        v[i] = Process(pid, insCount, IOper, startTime);
-    }
-    shortestRemainingTimeFirst(v, IOTime);
-    return 0;
-}
