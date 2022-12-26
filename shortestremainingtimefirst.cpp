097a018ae40ee625a3a8b435b0b4abedd7f6f1f1
Esraa said <66054841+Esraa-Said@users.noreply.github.com>
2022-12-26 22:33:23 +0200

    Update shortestremainingtimefirst.cpp

--- shortestremainingtimefirst.cpp
+++ shortestremainingtimefirst.cpp
@@ -1,6 +1,10 @@
 #include"STCF.h"
 void shortestRemainingTimeFirst(vector<Process>& v, ll IOTime, ll insTime)
 {
+    if (v.empty()) {
+        cout << "No processes\n";
+        return;
+    }
     priority_queue<Process, vector<Process>, SRTFcompare>app;
     map<ll, processMetaData>procData;
     for (Process& i : v)
@@ -8,50 +12,60 @@ void shortestRemainingTimeFirst(vector<Process>& v, ll IOTime, ll insTime)
         app.push(i);
         procData[i.pId].arrival = i.readyTime;
     }
-    ll cTime = 0;
-    pair<ll, ll>currentProc = { app.top().pId,app.top().readyTime };//id - when it take the cpu
+    ll cTime = 0, lft = 0;
+    pair<ll, ll>currentProc = { -1,0 };//id - when it take the cpu
     while (app.size())
     {
         Process proc = app.top();
         app.pop();
         if (proc.readyTime < cTime) {
+            //comming process readyTime is less than current time
             proc.readyTime = cTime;
             app.push(proc);
             continue;
         }
-        if (proc.pId != currentProc.first) {
-            print(currentProc.second, cTime, currentProc.first, procData[currentProc.first].completion != -1,0);
-            currentProc = make_pair(proc.pId, proc.readyTime);
-        }
         if (proc.readyTime > cTime)
-        {
-            print(cTime, proc.readyTime - 1, -1,0,0);
+        {//free cpu from cTime => readyTime
+            cout << "cpu is free now. current time = " << cTime << endl;
             cTime = proc.readyTime;
+            app.push(proc);
+            continue;
+        }
+        //check process context switch
+        if (proc.pId != currentProc.first) {
+            cout << "process " << proc.pId << " starts running. current Time = " << cTime << endl;
+            currentProc = make_pair(proc.pId, cTime);
         }
         if (procData[proc.pId].firstTun == -1)procData[proc.pId].firstTun = cTime;
         if (proc.insType[proc.insIdx] == 0)
-        {
+        {// cpu instruction
             proc.insIdx++;
             cTime += insTime;
             proc.readyTime = cTime;
             if (proc.insIdx != proc.numberOfInstructions)
                 app.push(proc);
-            if (proc.insIdx == proc.numberOfInstructions)
+            if (proc.insIdx == proc.numberOfInstructions) {
                 procData[proc.pId].completion = cTime;
+                cout << "Process is terminated. current Time = " << cTime << endl;
+            }
             continue;
         }
+        //io instruction
+        cout << "process "<<currentProc.first<<" issues IO at " << cTime << ".....\n";
+        currentProc.second = cTime;
         proc.insIdx++;
         cTime += insTime;
         proc.readyTime = cTime + IOTime;
-        print(proc.readyTime - 1, cTime, proc.pId, proc.insIdx == proc.numberOfInstructions,0);
         if (proc.insIdx != proc.numberOfInstructions)
             app.push(proc);
-        if (proc.insIdx == proc.numberOfInstructions)
+        if (proc.insIdx == proc.numberOfInstructions) {
             procData[proc.pId].completion = proc.readyTime;
+            cout << "process  terminates after IO at " << proc.readyTime << endl;
+        }
     }
-    print(currentProc.second, cTime, currentProc.first, 1,0);
+
     cout << endl;
+    procData.erase(-1);
     metaDataDisplay(procData, v.size());
 
 }
-
